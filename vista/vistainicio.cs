5697b0f2257b74536ecef4673aa1d6a51c21bbe6
fernando osorio carmona <fernandoantoniofaoc@gmail.com>
2024-01-15 15:24:05 -0300

    se crean carpetas

--- vista/vistainicio.cs
+++ vista/vistainicio.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace apescacces
+namespace apescacces.vista
 {
-    public partial class Form1 : Form
+    public partial class vistainicio : Form
     {
-        public Form1()
+        public vistainicio()
         {
             InitializeComponent();
         }
